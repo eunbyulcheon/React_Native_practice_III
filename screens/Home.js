b10930a47c372e2fe2d786b4507f2c7c9eec81f5
Eun Byul Cheon <75746836+eunbyulcheon@users.noreply.github.com>
2023-02-27 21:25:29 +0900

    "Add: Realm Schema"
    - Created an object schema that Realm will contain.
    - Sent the realm state to the other components through the useContext hook.

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,41 @@
-import React from 'react';
-import colors from '../colors';
+import React, { useState, useEffect } from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { useDB } from '../context';
+import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
+import colors from '../colors';
 
 const Home = ({ navigation: { navigate } }) => {
+	const realm = useDB();
+	const [feelings, setFeelings] = useState([]);
+
+	useEffect(() => {
+		const feelings = realm.objects('Feeling');
+		setFeelings(feelings);
+		feelings.addListener(() => {
+			const feelings = realm.objects('Feeling');
+			setFeelings(feelings);
+		});
+		return () => {
+			feelings.removeAllListeners();
+		};
+	}, []);
+
 	return (
 		<View>
 			<Title>My Journal</Title>
+			<FlatList
+				data={feelings}
+				contentContainerStyle={{ paddingVertical: 10 }}
+				ItemSeparatorComponent={Separator}
+				keyExtractor={(item) => item._id + ''}
+				renderItem={({ item }) => (
+					<Record>
+						<Emotion>{item.emotion}</Emotion>
+						<Message>{item.message}</Message>
+					</Record>
+				)}
+			/>
 			<Btn onPress={() => navigate('Write')}>
 				<Ionicons name="add" color="white" size={40} />
 			</Btn>
@@ -22,9 +51,31 @@ const View = styled.View`
 `;
 
 const Title = styled.Text`
-	margin-bottom: 100px;
+	margin-bottom: 80px;
 	color: ${colors.textColor};
 	font-size: 38px;
+`;
+
+const Record = styled.View`
+	flex-direction: row;
+	align-items: center;
+	padding: 10px 20px;
+	border-radius: 10px;
+	background-color: ${colors.cardColor};
+`;
+
+const Emotion = styled.Text`
+	margin-right: 10px;
+	font-size: 24px;
+`;
+
+const Message = styled.Text`
+	font-size: 18px;
+	font-weight: 400;
+`;
+
+const Separator = styled.View`
+	height: 10px;
 `;
 
 const Btn = styled.TouchableOpacity`
@@ -36,7 +87,7 @@ const Btn = styled.TouchableOpacity`
 	border-radius: 40px;
 	justify-content: center;
 	align-items: center;
-	box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+	box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
 	background-color: ${colors.btnColor};
 `;
 
--- screens/Write.js
+++ screens/Write.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Alert } from 'react-native';
 import styled from 'styled-components/native';
 import { useDB } from '../context';
